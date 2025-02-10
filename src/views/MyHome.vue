6d9989780d3bb42c373669e59b3975e37f0302b7
yassine <yassine.el-brahmi@eoa.de>
2025-02-10 12:15:12 +0100

    app new products and categories section

--- src/views/MyHome.vue
+++ src/views/MyHome.vue
@@ -4,6 +4,8 @@
       <TheFeaturesApp />
       <TopOffers />
       <FlashDeals :products="flashDeals" />
+      <TopCategories />
+      <NewProducts :products="newProducts"/>
     </div>
 </template>
 
@@ -12,6 +14,8 @@ import TheFeaturesApp from '@/components/Home-page/TheFeaturesApp.vue';
 import TopOffers from '@/components/Home-page/TopOffers.vue';
 import UpperBanner from '@/components/Home-page/UpperBanner.vue';
 import FlashDeals from '@/components/Home-page/FlashDeals.vue';
+import TopCategories from '@/components/Home-page/TopCategories.vue';
+import NewProducts from '@/components/Home-page/NewProducts.vue';
 import { mapActions, mapState } from 'pinia';
 import { useProductsStore } from '@/stores/products';
 export default {
@@ -21,9 +25,11 @@ export default {
         TheFeaturesApp,
         TopOffers,
         FlashDeals,
+        TopCategories,
+        NewProducts,
     },
     computed: {
-    ...mapState(useProductsStore, ['flashDeals']),
+    ...mapState(useProductsStore, ['flashDeals', 'newProducts']),
     },
     methods: {
         ...mapActions(useProductsStore, ['getProducts']),
