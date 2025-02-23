fb23c1dfa79fd43b4a2f3c2347f72c64be28b63f
Yassine <yassineelbrahmi11@gmail.com>
2025-02-23 16:11:09 +0100

    add quick view for every product on home page

--- src/components/Home-page/FlashDeals.vue
+++ src/components/Home-page/FlashDeals.vue
@@ -9,76 +9,149 @@
       :autoplay="{ delay: 3000 }"
     >
       <swiper-slide v-for="product in products" :key="product.id">
-        <v-card elevation="0" class="pa-5">
-          <img
-            :src="selectedImages[product.id] || product.thumbnail"
-            class="w-100"
-            style="height: 200px; object-fit: cover"
-          />
-          <v-card-text>
-            {{ truncateDescription(product.description) }}
-          </v-card-text>
-          <v-rating
-            :model-value="product.rating"
-            half-increments
-            readonly
-            color="yellow-darken-2"
-            size="x-small"
-            density="compact"
-          >
-          </v-rating>
-          <v-card-text class="pl-0 pt-0">
-            <del>${{ product.price }}</del> ${{
-              calculateDiscountedPrice(
-                product.price,
-                product.discountPercentage
-              )
-            }}
-          </v-card-text>
-          <v-btn-toggle
-            v-model="selectedImages[product.id]"
-            mandatory
-            class="overflow-x-auto"
-          >
-            <v-btn
-              v-for="(pic, i) in product.images"
-              :key="i"
-              :value="pic"
-              variant="text"
-            >
-              <img
-                :src="pic"
-                :alt="product.title"
-                width="50"
-                height="50"
-                style="object-fit: cover"
-              />
-            </v-btn>
-          </v-btn-toggle>
-          <div class="text-center mt-4">
+        <div class="card-wrapper">
+          <v-card elevation="0" class="pa-5" style="position: relative">
+            <img
+              :src="selectedImages[product.id] || product.thumbnail"
+              class="w-100"
+              style="height: 300px; object-fit: contain"
+            />
             <v-btn
               density="compact"
-              class="py-3 px-7 d-flex align-center justify-center"
+              width="80"
+              variant="outlined"
+              height="30"
+              class="bg-white quick-view"
               style="
                 text-transform: none;
+                position: absolute;
+                top: 35%;
+                left: 50%;
+                transform: translate(-50%, -50%);
                 border-radius: 30px;
-                min-width: 150px;
+                transition: 0.2s all ease-in-out;
               "
-              variant="outlined"
-              @click="navigateToProductDetails(product.id)"
+              @click="openQuickView(product)"
             >
-              Choose Options
+              Quick View
             </v-btn>
-          </div>
-        </v-card>
+            <v-card-text>
+              {{ truncateDescription(product.description) }}
+            </v-card-text>
+            <v-rating
+              :model-value="product.rating"
+              half-increments
+              readonly
+              color="yellow-darken-2"
+              size="x-small"
+              density="compact"
+            />
+            <v-card-text class="pl-0 pt-0">
+              <del>${{ product.price }}</del>
+              {{
+                calculateDiscountedPrice(
+                  product.price,
+                  product.discountPercentage
+                )
+              }}
+            </v-card-text>
+            <v-btn-toggle
+              v-model="selectedImages[product.id]"
+              mandatory
+              class="overflow-x-auto"
+            >
+              <v-btn
+                v-for="(pic, i) in product.images"
+                :key="i"
+                :value="pic"
+                variant="text"
+              >
+                <img
+                  :src="pic"
+                  :alt="product.title"
+                  width="50"
+                  height="50"
+                  style="object-fit: cover"
+                />
+              </v-btn>
+            </v-btn-toggle>
+            <div class="text-center mt-4">
+              <v-btn
+                density="compact"
+                class="py-3 px-7 d-flex align-center justify-center"
+                style="
+                  text-transform: none;
+                  border-radius: 30px;
+                  min-width: 150px;
+                "
+                variant="outlined"
+                @click="navigateToProductDetails(product.id)"
+              >
+                Choose Options
+              </v-btn>
+            </div>
+          </v-card>
+        </div>
       </swiper-slide>
       <div class="swiper-pagination"></div>
     </Swiper>
+
+    <!-- Quick View Dialog -->
+    <v-dialog v-model="quickViewOpen" max-width="600px">
+      <v-card>
+        <v-card-title justify="space-between">
+          {{ selectedProduct?.title }}
+          <v-btn icon class="float-right" @click="quickViewOpen = false">
+            <v-icon>mdi-close</v-icon>
+          </v-btn>
+        </v-card-title>
+        <v-card-text>
+          <img
+            :src="
+              selectedImages[selectedProduct?.id] || selectedProduct?.thumbnail
+            "
+            class="w-100 mb-4"
+            style="height: 300px; object-fit: cover"
+          />
+          <div>{{ selectedProduct?.description }}</div>
+          <div class="mt-2">
+            <v-rating
+              :model-value="selectedProduct?.rating"
+              half-increments
+              readonly
+              color="yellow-darken-2"
+              size="small"
+            />
+          </div>
+          <div class="mt-2">
+            <del>${{ selectedProduct?.price }}</del>
+            <span class="ml-2">
+              {{
+                calculateDiscountedPrice(
+                  selectedProduct?.price,
+                  selectedProduct?.discountPercentage
+                )
+              }}
+            </span>
+          </div>
+        </v-card-text>
+        <v-card-actions>
+          <v-btn text @click="quickViewOpen = false">Close</v-btn>
+          <v-btn
+            color="primary"
+            @click="navigateToProductDetails(selectedProduct?.id)"
+          >
+            View Details
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
+
 <script>
 import { Swiper, SwiperSlide } from "vue-awesome-swiper";
-import { Pagination, Navigation, Autoplay} from "swiper/modules";
+import { Pagination, Navigation, Autoplay } from "swiper/modules";
 export default {
   name: "FlashDeals",
   props: { products: { type: Array, required: true } },
@@ -89,7 +162,11 @@ export default {
   },
   components: { Swiper, SwiperSlide },
   data() {
-    return { selectedImages: {} };
+    return {
+      selectedImages: {},
+      quickViewOpen: false,
+      selectedProduct: null,
+    };
   },
   methods: {
     truncateDescription(description) {
@@ -102,16 +179,29 @@ export default {
       return Math.ceil(price - price * (discountPercentage / 100));
     },
     navigateToProductDetails(productId) {
-        this.$router.push({ name: 'ProductDetails', params: { id: productId } });
-    }
+      this.$router.push({ name: "ProductDetails", params: { id: productId } });
+    },
+    openQuickView(product) {
+      this.selectedProduct = product;
+      if (!this.selectedImages[product.id]) {
+        this.selectedImages[product.id] = product.thumbnail;
+      }
+      this.quickViewOpen = true;
+    },
   },
 };
 </script>
+
 <style scoped>
-pagination-bullets, .swiper-pagination-bullets.swiper-pagination-horizontal {
-    bottom: -6px;
+.card-wrapper {
+  position: relative;
 }
-.v-btn-group {
-    display: block;
+
+.card-wrapper:hover .quick-view {
+  opacity: 1;
+}
+
+.quick-view {
+  opacity: 0;
 }
 </style>
